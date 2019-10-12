c40744bde1718a5619d737d295b645f27a578c3c
Ilya Lisin <blacklacost@gmail.com>
2019-10-12 17:48:08 +0300

    init jinja2

--- app/server.py
+++ app/server.py
@@ -1,9 +1,13 @@
 import uvicorn
 from starlette.applications import Starlette
 from starlette.responses import JSONResponse
+from starlette.templating import Jinja2Templates
+
+templates = Jinja2Templates(directory='app/templates')
 
 app = Starlette(debug=True)
 
 @app.route('/')
 async def index(request):
-    return JSONResponse({'hello': 'world'})
+    context = {'request': request}
+    return templates.TemplateResponse('index.html', context)
